ad97c76efb97452909a037798e95a87ad0b6e39d
gnituy18 <sfhp@protonmail.com>
2021-11-01 20:18:21 +0800

    Fix Google login

--- web/src/lib/GoogleLoginButton.svelte
+++ web/src/lib/GoogleLoginButton.svelte
@@ -1,29 +1,22 @@
 <script lang="ts">
-	function load(_node: HTMLElement) {
+	function signIn() {
 		gapi.load('client:auth2', async () => {
 			try {
 				await gapi.client.init({
 					clientId: import.meta.env.VITE_CLIENT_ID,
 					scope: 'openid email profile'
 				});
+				await gapi.auth2.getAuthInstance().signIn();
+				console.log('signed');
 			} catch (err) {
 				console.log(err);
 			}
 		});
 	}
-
-	async function signIn() {
-		if (!gapi.auth2) {
-			return;
-		}
-
-		await gapi.auth2.getAuthInstance().signIn();
-		console.log('signed');
-	}
 </script>
 
 <svelte:head>
-	<script async defer src="https://apis.google.com/js/api.js" use:load></script>
+	<script async defer src="https://apis.google.com/js/api.js"></script>
 </svelte:head>
 
 <button on:click={signIn}> Log in </button>
