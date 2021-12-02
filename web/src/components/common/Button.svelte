34dd29cbc6ce19e088586694b61a6800dd0a1e28
gnituy18 <sfhp@protonmail.com>
2021-12-03 01:45:54 +0800

    Add mission list

--- web/src/components/common/Button.svelte
+++ web/src/components/common/Button.svelte
@@ -4,7 +4,7 @@
   export let onClick: () => void = () => {};
 </script>
 
-<button class="w-full rounded pt-2 pb-2 text-gray-500 bg-gray-300 hover:bg-gray-200"  on:click={onClick}>
+<button class="w-full rounded pt-2 pb-2 text-gray-500 bg-gray-300 hover:bg-gray-200" on:click={onClick}>
   {#if href !== ""}
     <a {href}>
       {value}
--- web/src/components/header/Header.svelte
+++ web/src/components/header/Header.svelte
@@ -1,7 +1,14 @@
 <script>
+  import { onMount } from "svelte";
   import { session } from "$app/stores";
 
   import Avatar from "$components/common/Avatar.svelte";
+
+  let missions = [];
+  onMount(async () => {
+    const resp = await fetch("http://localhost:8080/api/v1/mission", { credentials: "include" });
+    missions = await resp.json();
+  });
 
   let user;
   session.subscribe(({ currentUser }) => {
@@ -20,5 +27,12 @@
       <h2>Missions</h2>
       <a href="/mission/create" class="font-medium">+</a>
     </div>
+    {#each missions as { id, name }}
+      <li>
+        <a target="_blank" href="/mission/{id}">
+          {name}
+        </a>
+      </li>
+    {/each}
   </nav>
 </header>
