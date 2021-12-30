f9c604a32b357b4e28ec4d7a8b76ea6a99b067c4
gnituy18 <sfhp@protonmail.com>
2021-12-31 02:16:13 +0800

    Hide logo circle

--- web/src/components/header/Logo.svelte
+++ web/src/components/header/Logo.svelte
@@ -1,8 +1,7 @@
 <div class="flex items-center w-full">
   <a href="/">
     <div class="group flex items-center gap-1">
-      <div class="w-12 h-12 rounded-full bg-gray-200 group-hover:bg-blue-200" />
-      <h1>Gritter</h1>
+      <h1 class="underline decoration-double text-slate-600">Gritter</h1>
     </div>
   </a>
 </div>
