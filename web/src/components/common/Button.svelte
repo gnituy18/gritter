d239386377075e1881bab4c701ca162c2de44ddd
gnituy18 <sfhp@protonmail.com>
2021-12-31 02:22:53 +0800

    fix button bug

--- web/src/components/common/Button.svelte
+++ web/src/components/common/Button.svelte
@@ -4,15 +4,11 @@
   export let onClick: () => void = () => {};
 </script>
 
-<button
-  class="border border-slate-500 rounded-full px-3 text-slate-500 bg-slate-300 hover:bg-slate-200"
-  on:click={onClick}
->
-  {#if href !== ""}
-    <a {href}>
-      {value}
-    </a>
-  {:else}
+<a {href}>
+  <button
+    class="border border-slate-500 rounded-full px-3 text-slate-500 bg-slate-300 hover:bg-slate-200"
+    on:click={onClick}
+  >
     {value}
-  {/if}
-</button>
+  </button>
+</a>
